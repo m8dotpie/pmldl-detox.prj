280a4f34c9fbe5976970cfa5864e1ef1186753ac
m8dotpie <alentev.olm@ya.ru>
2023-10-29 15:08:03 +0300

    feat: bad words dataset

--- src/data/fetch_external.py
+++ src/data/fetch_external.py
@@ -22,4 +22,15 @@ def fetch_embeddings(url=None):
             f.write(response.content)
 
 
-fetch_embeddings()
+def fetch_badwords(url=None):
+    """Fetches the badwords from the web and saves them locally."""
+
+    if url is None:
+        url = "https://gist.githubusercontent.com/m8dotpie/b2d9afcacb1c447c5dc4d509f7007470/raw/c18ccc661ee83fba81a1522543cc5450c276971c/bad_words.csv"
+
+    filename = root + "/" + os.getenv("DATA_DIR") + "/external/" + "bad-words.csv"
+    response = requests.get(url, timeout=100)
+
+    if response.ok:
+        with open(filename, "wb") as f:
+            f.write(response.content)
