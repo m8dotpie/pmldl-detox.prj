fb2ab7f9ea2b39a4f78a1c13181e2c9cfe887b63
m8dotpie <alentev.olm@ya.ru>
2023-11-02 16:51:57 +0300

    fix: finalize visualization

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,12 +1,14 @@
+"""Visualization module used for data visualization on evaluation"""
+
+import os
+import sys
 import numpy as np
-import pandas as pd
 import seaborn as sns
 from wordcloud import WordCloud
 from matplotlib import pyplot as plt
 
 from dotenv import load_dotenv, find_dotenv
-import os
-import sys
+
 
 load_dotenv()
 root = os.path.dirname(find_dotenv())
@@ -14,6 +16,7 @@ sys.path.append(root)
 
 
 def get_wordcloud(text):
+    """Generate preset wordcloud"""
     wordcloud = WordCloud(
         width=300, height=300, background_color="white", margin=0, max_font_size=40
     ).generate(text)
@@ -24,11 +27,40 @@ def get_wordcloud(text):
 def plot_wordcloud(cloud, ax=None):
     """Plot the given wordcloud"""
     if ax is None:
-        fig, ax = plt.subplots(figsize=(8, 8), dpi=100)
+        _, ax = plt.subplots(figsize=(8, 8), dpi=100)
     ax.imshow(cloud, interpolation="bilinear")
     ax.axis("off")
     return ax
 
 
-def get_violin_plot():
-    pass
+def plot_hist(
+    metrics: dict,
+    xlabel="Toxicity",
+    ylabel="Frequency",
+    plot_title="Comparison of Toxicity Distribution",
+):
+    """Plot histogram of given metrics"""
+    titles, data = zip(*metrics.items())
+    colors = ["red", "green", "skyblue"]
+
+    for i, (title, d) in enumerate(zip(titles, data)):
+        plt.hist(d, bins=15, alpha=0.5, color=colors[i], edgecolor="black", label=title)
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(plot_title)
+    plt.legend(loc="upper right")
+
+    return plt
+
+
+def get_violin_plot(metrics: dict):
+    """Generate violin plot of given metrics"""
+    fig, ax = plt.subplots(len(metrics), 1, figsize=(5, 5 * len(metrics)))
+    ax = np.array(ax).flatten()
+
+    for i, (title, d) in enumerate(metrics.items()):
+        sns.violinplot(data=d, ax=ax[i], inner="quart")
+        ax[i].set_title(title)
+
+    return fig
