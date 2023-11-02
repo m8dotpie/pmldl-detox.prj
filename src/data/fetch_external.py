4bde7567101e09e4b51a48965e2e18b652efbc75
m8dotpie <alentev.olm@ya.ru>
2023-11-02 15:21:48 +0300

    fix: proper fetch

--- src/data/fetch_external.py
+++ src/data/fetch_external.py
@@ -1,8 +1,9 @@
 """Fetch heavy external resources"""
 
+import os
 import requests
 from dotenv import find_dotenv, load_dotenv
-import os
+
 
 load_dotenv()
 root = os.path.dirname(find_dotenv())
@@ -12,7 +13,7 @@ def fetch_embeddings(url=None):
     """Fetches the embeddings from the web and saves them locally."""
 
     if url is None:
-        url = "http://nlp.uoregon.edu/download/embeddings/glove.6B.100d.txt"
+        url = os.getenv("GLOVE_URL")
 
     filename = root + "/" + os.getenv("DATA_DIR") + "/external/" + "embeddings.txt"
     response = requests.get(url, timeout=100)
@@ -26,7 +27,7 @@ def fetch_badwords(url=None):
     """Fetches the badwords from the web and saves them locally."""
 
     if url is None:
-        url = "https://gist.githubusercontent.com/m8dotpie/b2d9afcacb1c447c5dc4d509f7007470/raw/c18ccc661ee83fba81a1522543cc5450c276971c/bad_words.csv"
+        url = os.getenv("BADWORDS_URL")
 
     filename = root + "/" + os.getenv("DATA_DIR") + "/external/" + "bad-words.csv"
     response = requests.get(url, timeout=100)
