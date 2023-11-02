5dc40966fd14df6b0e06e7f1e08c21c16417ffea
m8dotpie <alentev.olm@ya.ru>
2023-11-02 16:42:07 +0300

    fix: visualize

---
+++ src/visualization/visualize.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from wordcloud import WordCloud
+from matplotlib import pyplot as plt
+
+from dotenv import load_dotenv, find_dotenv
+import os
+import sys
+
+load_dotenv()
+root = os.path.dirname(find_dotenv())
+sys.path.append(root)
+
+
+def get_wordcloud(text):
+    wordcloud = WordCloud(
+        width=300, height=300, background_color="white", margin=0, max_font_size=40
+    ).generate(text)
+
+    return wordcloud
+
+
+def plot_wordcloud(cloud, ax=None):
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(8, 8), dpi=100)
+    ax.imshow(cloud, interpolation="bilinear")
+    ax.axis("off")
+    return ax
+
+
+def get_violin_plot():
+    pass
